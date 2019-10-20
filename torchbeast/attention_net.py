e7f1f2d8249176a1ed5eddac2a20e8664af4ac23
cjlovering <cjlovering@wpi.edu>
2019-10-19 23:18:05 -0400

    update to use bool tensor (as in upstream)

--- torchbeast/attention_net.py
+++ torchbeast/attention_net.py
@@ -116,7 +116,7 @@ class AttentionNet(nn.Module):
         # [N, h, w, num_values] -> [T, B, h, w, num_values]
         V = V.view(T, B, h, w, -1)
         # -> [T, B, 1]
-        notdone = (1 - inputs["done"].float()).view(T, B, 1)
+        notdone = (~inputs["done"].float()).view(T, B, 1)
         # -> [T, B, 1, num_actions]
         prev_action = (
             F.one_hot(inputs["last_action"].view(T * B), self.num_actions)
@@ -358,7 +358,7 @@ class VisionNetwork(nn.Module):
         _, C, H, W = X.size()
         X = X.view(T, B, C, H, W)
         output_list = []
-        notdone = 1 - inputs["done"].float()
+        notdone = (~inputs["done"]).float()
 
         # Operating over T:
         for X_t, nd in zip(X.unbind(), notdone.unbind()):
